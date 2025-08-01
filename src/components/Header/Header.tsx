d80128f158ebd3e9c05fc4a17361e10b6499e388
DianaAraujoG <dianac251@gmail.com>
2025-08-01 11:45:30 -0600

    UPDATE(header)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,31 @@
 import React from "react";
+import type { HeaderProps } from "./Header.types";
 
-export const Header: React.FC = () => {
+export const Header: React.FC<HeaderProps> = ({ onCreate }) => {
   return (
-    <header className="flex justify-between items-center">
-      <h1>TODO</h1>
-      <button>+</button>
+    <header className="flex justify-between items-center p-8">
+      <h1 className="font-bold text-3xl">TODO</h1>
+      <button
+        type="button"
+        title="Crear nota"
+        onClick={onCreate}
+        className="text-gray-700 hover:ring-2  focus:ring-2 focus:outline-none focus:ring-blue-300 rounded-full text-sm p-2.5 text-center inline-flex items-center me-2"
+      >
+        <svg
+          className="w-7 h-7"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth={2}
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 4v16m8-8H4"
+          />
+        </svg>
+      </button>
     </header>
   );
 };
